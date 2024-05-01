3568376637f447e81804c46d833b2efe13b20dc5
Nandini <115989171+nayandiniii@users.noreply.github.com>
2024-05-01 23:05:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,61 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchJobs } from "./actions/jobActions";
-import JobCard from "./components/JobCard";
-import Filters from "./components/Filters";
-import "./App.css";
+// App.js
 
-function App() {
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Grid } from "@material-ui/core";
+import JobCard from "./Card";
+
+const App = () => {
   const dispatch = useDispatch();
   const jobs = useSelector((state) => state.jobs);
-  const [filters, setFilters] = useState({
-    minExperience: 0,
-    company: "",
-    location: "",
-    remote: false,
-    techStack: "",
-    role: "",
-    minBasePay: 0,
-  });
 
   useEffect(() => {
-    dispatch(fetchJobs({ limit: 10, offset: 0 }));
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(
+          "https://api.weekday.technology/adhoc/getSampleJdJSON",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              limit: 10,
+              offset: 0,
+            }),
+          }
+        );
+        const data = await response.json();
+        dispatch({ type: "FETCH_JOBS_SUCCESS", payload: data });
+      } catch (error) {
+        dispatch({ type: "FETCH_JOBS_FAILURE", payload: error.message });
+      }
+    };
+
+    fetchJobs();
   }, [dispatch]);
 
-  const loadMoreJobs = () => {
-    dispatch(fetchJobs({ limit: 10, offset: jobs.length }));
-  };
-
-  const applyFilters = () => {
-    dispatch(fetchJobs({ ...filters, limit: 10, offset: 0 }));
-  };
-
-  const handleFilterChange = (name, value) => {
-    setFilters({ ...filters, [name]: value });
-  };
-
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Candidate Application Platform</h1>
-      </header>
-      <div className="app-container">
-        <Filters
-          filters={filters}
-          onFilterChange={handleFilterChange}
-          onApplyFilters={applyFilters}
-        />
-        <div className="job-list">
-          {jobs.map((job) => (
-            <JobCard key={job.id} job={job} />
-          ))}
-          <div className="load-more" onClick={loadMoreJobs}>
-            Load More
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        {jobs.map((job) => (
+          <Grid item key={job.id} xs={12} sm={6} md={4}>
+            <JobCard job={job} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
-}
+};
 
 export default App;
